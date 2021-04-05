715f7f7aa166cd74f551dd439acbe9dfd01e575f
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2021-04-05 21:48:17 +0200

    added tests for key types

--- tests/unit/Get-PSScriptInfoLegacy.Tests.ps1
+++ tests/unit/Get-PSScriptInfoLegacy.Tests.ps1
@@ -11,7 +11,70 @@ Describe -Name 'Get-PSScriptInfoLegacy.ps1' -Fixture {
             Set-Content -Path $file.fullname -Value "<#PSScriptInfo`n`r.VERSION 1.0.0.0`n`r.AUTHOR John Doe`n`r#>`r`nGet-Test`r`n"            
         }
         It -Name 'Should not throw' {
-            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -not -Throw
+            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -Not -Throw
+        }
+    }
+    Context 'When PSScriptInfo contains key CREATEDDATE' {
+        BeforeAll {
+            $file = New-Item -Path TestDrive:\file.ps1
+            Set-Content -Path $file.fullname -Value "<#PSScriptInfo`n`r.CREATEDDATE 2020-01-01`n`r#>`r`nGet-Test`r`n"            
+        }
+        It -Name 'Should not throw' {
+            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -Not -Throw
+        }
+    }
+    Context 'When PSScriptInfo contains key GUID' {
+        BeforeAll {
+            $file = New-Item -Path TestDrive:\file.ps1
+            Set-Content -Path $file.fullname -Value "<#PSScriptInfo`n`r.GUID e8b0cd33-7895-451a-b08c-92529596d26e`n`r#>`r`nGet-Test`r`n"            
+        }
+        It -Name 'Should not throw' {
+            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -Not -Throw
+        }
+    }
+    Context 'When PSScriptInfo contains key UNITTEST false' {
+        BeforeAll {
+            $file = New-Item -Path TestDrive:\file.ps1
+            Set-Content -Path $file.fullname -Value "<#PSScriptInfo`n`r.UNITTEST false`n`r#>`r`nGet-Test`r`n"            
+        }
+        It -Name 'Should not throw' {
+            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -Not -Throw
+        }
+    }
+    Context 'When PSScriptInfo contains key UNITTEST true' {
+        BeforeAll {
+            $file = New-Item -Path TestDrive:\file.ps1
+            Set-Content -Path $file.fullname -Value "<#PSScriptInfo`n`r.UNITTEST true`n`r#>`r`nGet-Test`r`n"            
+        }
+        It -Name 'Should not throw' {
+            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -Not -Throw
+        }
+    }
+    Context 'When PSScriptInfo contains key UNITTEST foo' {
+        BeforeAll {
+            $file = New-Item -Path TestDrive:\file.ps1
+            Set-Content -Path $file.fullname -Value "<#PSScriptInfo`n`r.UNITTEST foo`n`r#>`r`nGet-Test`r`n"            
+        }
+        It -Name 'Should not throw' {
+            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -Not -Throw
+        }
+    }
+    Context 'When PSScriptInfo contains key with string array' {
+        BeforeAll {
+            $file = New-Item -Path TestDrive:\file.ps1
+            Set-Content -Path $file.fullname -Value "<#PSScriptInfo`n`r.ARRAY foo,bar`n`r#>`r`nGet-Test`r`n"            
+        }
+        It -Name 'Should not throw' {
+            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -Not -Throw
+        }
+    }
+    Context 'When PSScriptInfo contains key without value' {
+        BeforeAll {
+            $file = New-Item -Path TestDrive:\file.ps1
+            Set-Content -Path $file.fullname -Value "<#PSScriptInfo`n`r.NONE `n`r#>`r`nGet-Test`r`n"            
+        }
+        It -Name 'Should not throw' {
+            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -Not -Throw
         }
     }
 }
