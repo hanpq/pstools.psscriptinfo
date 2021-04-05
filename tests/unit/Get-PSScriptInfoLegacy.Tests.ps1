00183f805c426f708c6f57e2f41f58e8d707baef
Hannes Palmquist <33247576+hanpq@users.noreply.github.com>
2021-04-05 21:21:14 +0200

    Added tests for get-psscriptinflegacy

---
+++ tests/unit/Get-PSScriptInfoLegacy.Tests.ps1
@@ -0,0 +1,17 @@
+﻿BeforeAll {
+    . (Resolve-Path -Path "$PSScriptRoot\..\..\source\private\Get-PSScriptInfoLegacy.ps1")
+}
+
+Describe -Name 'Get-PSScriptInfoLegacy.ps1' -Fixture {
+    BeforeAll {
+    }
+    Context 'When valid legacy PSScriptInfo is provided in file' {
+        BeforeAll {
+            $file = New-Item -Path TestDrive:\file.ps1
+            Set-Content -Path $file.fullname -Value "<#PSScriptInfo`n`r.VERSION 1.0.0.0`n`r.AUTHOR John Doe`n`r#>`r`nGet-Test`r`n"            
+        }
+        It -Name 'Should not throw' {
+            { Get-PSScriptInfoLegacy -FilePath $file.fullname } | Should -not -Throw
+        }
+    }
+}
